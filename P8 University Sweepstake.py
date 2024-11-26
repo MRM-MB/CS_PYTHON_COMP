fb37a023244e08bcfe29fea30899dfb14931c708
Manish Raj <manishmoriche@gmail.com>
2024-11-26 19:10:48 +0100

    Update P8 University Sweepstake.py

--- P8 University Sweepstake.py
+++ P8 University Sweepstake.py
@@ -1,31 +1,40 @@
-# Checker for the right input
+"""
+
+# STEP 1: [1, 2, 3, 4, 5, 6, 7, 8] -> Left To Right and take out odd postion (since we start from the first number on the left)
+
+# STEP 2: [2, 4, 6, 8] -> Right To Left (Transverse) and take out again odd position (starting from the first postion on the right)
+
+[2, 6] -> Repeat STEPS 1 and 2 as necessary to get the winner
+
+[6] -> WINNER
+
+"""
+
+def WinnerWinnerChickenDinner(winner):
+    # Left to Right movement for the list of numbers
+    LeftToRight = True
+    start = 1
+    move = 1
+    StudentsNumbers = winner # All the students receive a number assigned to them which is stored in an ordered array
+    
+    while StudentsNumbers > 1:
+        if LeftToRight or StudentsNumbers % 2 == 1: # Check for the direction and odd number for the remaining students
+            # Update start when from Left to Right
+            start += move
+        # Double the move and halve the StudentsNumbers
+        move *= 2
+        StudentsNumbers //= 2
+        # Switch direction
+        LeftToRight = not LeftToRight
+    return start
+
+# Take an input
 while True:
-    user_input = input("Please insert a number(int): ")
     try:
-        n = int(user_input)
-        if(n > 0):
+        winner = int(input("\nEnter number of students (int): "))
+        if winner > 0:
             break
+        print("\nThe number must be greater than 0")
     except ValueError:
-        print("Error: This is not an integer")
-
-def WinnerWinnerChickenDinner(n):
-    # List of StudentsParticipants
-    StudentsParticipants = list(range(1, n + 1))
-    LeftRight = True # Control Direction
-    
-    while len(StudentsParticipants) > 1:
-        if LeftRight:
-            # Eliminate from LeftRight
-            StudentsParticipants = [StudentsParticipants[i] for i in range(len(StudentsParticipants)) if i % 2 == 1]
-        else:
-            # Eliminate from RightLeft
-            StudentsParticipants = [StudentsParticipants[i] for i in range(len(StudentsParticipants)) if (len(StudentsParticipants) - i - 1) % 2 == 1]
-        
-        # Switch Direction
-        LeftRight = not LeftRight
-    
-    # Left over student -> Winner
-    return StudentsParticipants[0]
-
-winner = WinnerWinnerChickenDinner(n)
-print(winner)
+        print("\nThe number must be an integer 🤔")
+print("\n🎉 🥳 Congratulations, you won student number:", WinnerWinnerChickenDinner(winner))
